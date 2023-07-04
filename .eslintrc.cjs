87087d09aeb610faea0b1e2affe566349ea926cb
Debbl <me@aiwan.run>
2023-07-04 19:43:18 +0800

    chore: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    "next/core-web-vitals",
+    "@debbl/eslint-config-tailwindcss",
+    "@debbl/eslint-config-react",
+    "@debbl/eslint-config-prettier",
+  ],
+};
