31ac2c8711610f8d3385d8115c7ec86bfa1d6a1a
Debbl <me@aiwan.run>
2023-07-15 13:42:57 +0800

    chore: add devtools and persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 import { getCoverAction } from "~/actions/getCoverAction";
 import { getShortLinkAction } from "~/actions/getShortLinkAction";
 
@@ -31,56 +32,77 @@ interface GetCoverStore {
   };
 }
 
-const useGetShortUrlStore = create<GetShortUrlStore>()((set, get) => ({
-  url: "https://space.bilibili.com/174865648?spm_id_from=333.1007.0.0",
-  shortUrl: "",
-  isCopy: false,
-  setIsCopy: (isCopy: boolean) => set({ isCopy }),
-  setUrl: (url: string) => set({ url }),
-  setShortUrl: (shortUrl: string) => set({ shortUrl }),
-  getShortUrl: async () => {
-    if (!get().url) return;
+const useGetShortUrlStore = create<GetShortUrlStore>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        url: "https://space.bilibili.com/174865648?spm_id_from=333.1007.0.0",
+        shortUrl: "",
+        isCopy: false,
+        setIsCopy: (isCopy: boolean) => set({ isCopy }),
+        setUrl: (url: string) => set({ url }),
+        setShortUrl: (shortUrl: string) => set({ shortUrl }),
+        getShortUrl: async () => {
+          if (!get().url) return;
 
-    const res = await getShortLinkAction(get().url);
-    const shortUrlRes = res?.data.content;
+          const res = await getShortLinkAction(get().url);
+          const shortUrlRes = res?.data.content;
 
-    if (!shortUrlRes) {
-      // eslint-disable-next-line no-alert
-      alert("url为空或解析错误");
-      return;
-    }
+          if (!shortUrlRes) {
+            // eslint-disable-next-line no-alert
+            alert("url为空或解析错误");
+            return;
+          }
 
-    set({ shortUrl: shortUrlRes });
-  },
-}));
+          set({ shortUrl: shortUrlRes });
+        },
+      }),
+      {
+        name: "bili-get-short-url-store",
+      }
+    )
+  )
+);
 
-const useGetCoverStore = create<GetCoverStore>()((set, get) => ({
-  videoUrl:
-    "https://www.bilibili.com/video/BV16X4y1H7LR/?vd_source=846e45e6e150f0469fe98e948cf11679",
-  videoInfo: {
-    pic: "",
-    title: "",
-    img: "",
-  },
-  computed: {
-    get vid() {
-      return /\/(BV\w+)\\?/.exec(get()?.videoUrl)?.[1];
-    },
-  },
-  setVideoUrl: (videoUrl: string) => set({ videoUrl }),
-  setVideoInfo: (videoInfo: { pic: string; title: string; img: string }) =>
-    set({ videoInfo }),
-  getCover: () => {
-    const vid = get().computed.vid;
-    vid &&
-      getCoverAction(vid).then((d) => {
-        if (!d) {
-          // eslint-disable-next-line no-alert
-          alert("解析错误！");
-          set({ videoInfo: { pic: "", title: "", img: "" } });
-        } else set({ videoInfo: { ...d.data } });
-      });
-  },
-}));
+const useGetCoverStore = create<GetCoverStore>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        videoUrl:
+          "https://www.bilibili.com/video/BV16X4y1H7LR/?vd_source=846e45e6e150f0469fe98e948cf11679",
+        videoInfo: {
+          pic: "",
+          title: "",
+          img: "",
+        },
+        computed: {
+          get vid() {
+            return /\/(BV\w+)\\?/.exec(get()?.videoUrl)?.[1];
+          },
+        },
+        setVideoUrl: (videoUrl: string) => set({ videoUrl }),
+        setVideoInfo: (videoInfo: {
+          pic: string;
+          title: string;
+          img: string;
+        }) => set({ videoInfo }),
+        getCover: () => {
+          const vid = get().computed.vid;
+          vid &&
+            getCoverAction(vid).then((d) => {
+              if (!d) {
+                // eslint-disable-next-line no-alert
+                alert("解析错误！");
+                set({ videoInfo: { pic: "", title: "", img: "" } });
+              } else set({ videoInfo: { ...d.data } });
+            });
+        },
+      }),
+      {
+        name: "bili-get-cover-store",
+      }
+    )
+  )
+);
 
 export { useGetShortUrlStore, useGetCoverStore };
